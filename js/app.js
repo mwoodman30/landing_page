16777c97cc64c77099443ec832742316dcbe1451
Mary Anne Woodman <42190035+mwoodman30@users.noreply.github.com>
2021-01-02 10:05:17 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,75 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
 
+/**
+ * Define Global Variables
+ * 
+*/
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+
+
+// Add class 'active' to section when near top of viewport
+
+
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+// Scroll to section on link click
+
+// Set sections as active
+ // Build Dynamic Navbar
+ 
+ const menu = document.querySelector('mobile-menu');
+ const menuLinks = document.querySelector('.nav-menu');
+
+ menu.addEventListener('click', function() {
+     menu.classList.toggle('is-active');
+     menuLinks.classList.toggle('active');
+
+ })
+
+ window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("nav-menu").style.top = "0";
+  } else {
+    document.getElementById("nav-menu").style.top = "-50px";
+  }
+}
