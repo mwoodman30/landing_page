6062b61aa54d1170d37bdd8500698b77963aea39
Mary Anne Woodman <42190035+mwoodman30@users.noreply.github.com>
2021-01-10 14:49:15 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+// navbar 
 const slide = () => {
     const bar = document.querySelector('.bar');
     const nav = document.querySelector('.nav-links');
@@ -22,6 +22,7 @@ const slide = () => {
 }
 slide();
 
+//scroll function
 const navLinks = document.querySelectorAll('a')
 
 function smoothScroll() {
@@ -29,7 +30,6 @@ function smoothScroll() {
         link.addEventListener('click', function (e) {
             const LinkAttribut = link.getAttribute("href").substring(1);
             e.preventDefault();
-            //prevent the default action of a click, allow to change the behaviour, in this case it will allow to scroll
             const sections = document.querySelectorAll('section');
             sections.forEach((section) => {
                 const sectionId = section.id;
@@ -42,4 +42,26 @@ function smoothScroll() {
         })
     })
 }
-smoothScroll()
+smoothScroll();
+
+
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
