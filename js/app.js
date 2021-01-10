6258817df80a289c142a6f7787e7c4d10d6154c8
Mary Anne Woodman <42190035+mwoodman30@users.noreply.github.com>
2021-01-10 12:41:40 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,75 +1,45 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
 
-/**
- * Define Global Variables
- * 
-*/
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
- // Build Dynamic Navbar
- 
- const menu = document.querySelector('mobile-menu');
- const menuLinks = document.querySelector('.nav-menu');
-
- menu.addEventListener('click', function() {
-     menu.classList.toggle('is-active');
-     menuLinks.classList.toggle('active');
-
- })
-
- window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("nav-menu").style.top = "0";
-  } else {
-    document.getElementById("nav-menu").style.top = "-50px";
-  }
+const slide = () => {
+    const bar = document.querySelector('.bar');
+    const nav = document.querySelector('.nav-links');
+    const navLinks =document.querySelectorAll('.nav-links li');
+    
+    //toggle nav
+    bar.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+     //animate links
+    navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 1}s`;
+        }
+    });   
+    //bar animation
+    bar.classList.toggle('toggle');
+    });
+    
 }
+slide();
+
+const navLinks = document.querySelectorAll('a')
+
+function smoothScroll() {
+    navLinks.forEach((link) => {
+        link.addEventListener('click', function (e) {
+            const LinkAttribut = link.getAttribute("href").substring(1);
+            e.preventDefault();
+            //prevent the default action of a click, allow to change the behaviour, in this case it will allow to scroll
+            const sections = document.querySelectorAll('section');
+            sections.forEach((section) => {
+                const sectionId = section.id;
+                if (LinkAttribut === sectionId) {
+                    section.scrollIntoView({
+                        behavior: 'smooth'
+                    })
+                }
+            })
+        })
+    })
+}
+smoothScroll()
