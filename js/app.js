e9e7f13110f886c9ed128a266aa3f0202bbdc23d
Mary Anne Woodman <42190035+mwoodman30@users.noreply.github.com>
2021-01-16 09:45:35 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,45 +1,59 @@
-//navbar appear
-function createNav(name) {
+const navlink = document.getElementById('navlinks');
+const navList = ['Section 1', 'Section 2', 'Section 3', 'Section 4'];
 
-let navbar = document.createElement('li');
-navbar.textContent = name;
-return ListeningStateChangedEvent;
-}
-const nav = document.querySelector('.nav-links');
-navlinks.appendChild(createNav('#section1', '#section2', '#section3', '#secton4'));
+   for (let section of navList) {
+       let SectionList = document.createElement('li');
+       SectionList.textContent = section;
+       navlinks.appendChild(SectionList);
+      
+   }
+   
+/********************************/
+const fragment = new DocumentFragment();
 
+navList.forEach(function (navlink) {
+    const a = document.createElement('a');
+    a.innerHTML = item;
+    a.setAttribute('href', `#${navlink}`);
+    const navbar = document.createElement('li');
+    navbar.appendChild(a);
+    fragment.appendChild(navbar);
+})
 
-/********* BELOW CODE WORKS WHEN HTML CODE "LI" IS NOT COMMENTED OUT, 
- ABOVE CODE IS THE PROBLEM  ************/
+navlink.appendChild(fragment);
 
-
-
-// navbar 
-const slide = () => {
+/********************************/ 
+// navbar mobile menu
+function slide() {
   const bar = document.querySelector('.bar');
-  const nav = document.querySelector('.nav-links');
-  const navLinks =document.querySelectorAll('.nav-links li');
+  const nav = document.querySelector('.navlinks');
+  const navLinks = document.querySelectorAll('.navlinks li');
   
   //toggle nav
   bar.addEventListener('click', () => {
-      nav.classList.toggle('nav-active');
-   //animate links
-  navLinks.forEach((link, index) => {
+    nav.classList.toggle('nav-active');
+    //animate links
+    navLinks.forEach((link, index) => {
       if (link.style.animation) {
-          link.style.animation = '';
+        link.style.animation = '';
       } else {
-      link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 1}s`;
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
       }
-  });   
-  //bar animation
-  bar.classList.toggle('toggle');
+    });
+    //bar animation
+    bar.classList.toggle('toggle');
   });
-  
+
 }
 slide();
 
+// mobile bars
+function bar(x) {
+  x.classList.toggle("toggle");
+}
+
 //scroll function
-const navLinks = document.querySelectorAll('a')
+const navLinks = document.querySelectorAll('li')
 
 function smoothScroll() {
   navLinks.forEach((link) => {
@@ -73,9 +87,36 @@ if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
   mybutton.style.display = "none";
 }
 }
-
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
 document.body.scrollTop = 0; // For Safari
 document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+
+/*    HIGHLIGHT NAVBAR   
+let navigLink = document.querySelectorAll('section');
+let sectionList = document.querySelectorAll('#sectionBody');
+
+window.addEventListener('scroll', function(){
+    for (list of sectionList) {
+      let position = list.getBoundingClientRect();
+
+      if (position.top <= 150 && position.bottom >= 150) {
+        list.classList.add('active-section');
+        for (const navig of navigLink) {
+          if(navig.classList.contains(list.id)) {
+            navig.classList.add('active-nav');
+          }
+        }
+      } else {
+        for (const navig of navigLink) {
+          list.classList.remove('active-section');
+          if (navig.classList.contains(list.id)){
+            navig.classList.remove('active-nav');
+          }
+        }
+      }
+    }
+});*/
